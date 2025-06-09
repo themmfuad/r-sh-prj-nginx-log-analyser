cbbcc6e3aa8c422425db9124ce210afda1159415
themmfuad <123576984+themmfuad@users.noreply.github.com>
2025-06-09 17:51:08 +0600

    implemented top 5 paths segment

--- log-analyser.sh
+++ log-analyser.sh
@@ -1,3 +1,9 @@
+# top 5 ip addresses
 echo Top 5 IP addresses with the most requests: 
-
 awk '{print $1}' nginx-access.log | sort | uniq -c | sort -nr | head -n5 | awk '{print $2 " - " $1 " requests"}'
+
+# top 5 paths
+echo Top 5 most requested paths:
+awk '{print $7}' nginx-access.log | sort | uniq -c | sort -nr | head -n5 | awk '{print $2 " - " $1 " requests"}'
+
+#
