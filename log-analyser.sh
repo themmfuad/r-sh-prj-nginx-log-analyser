6e1f93aa74a56f5cb6e2634e57c8981062d6a481
themmfuad <123576984+themmfuad@users.noreply.github.com>
2025-06-09 22:07:28 +0600

    competed the script with top 5 response status codes

--- log-analyser.sh
+++ log-analyser.sh
@@ -1,9 +1,12 @@
 # top 5 ip addresses
-echo Top 5 IP addresses with the most requests: 
+echo "Top 5 IP addresses with the most requests:"
 awk '{print $1}' nginx-access.log | sort | uniq -c | sort -nr | head -n5 | awk '{print $2 " - " $1 " requests"}'
 
 # top 5 paths
-echo Top 5 most requested paths:
+echo "\nTop 5 most requested paths:"
 awk '{print $7}' nginx-access.log | sort | uniq -c | sort -nr | head -n5 | awk '{print $2 " - " $1 " requests"}'
 
-#
+# Top 5 response status codes:
+echo "\nTop 5 response status codes:"
+awk -F'"' '{print $3}' nginx-access.log | awk '{print $1}' | sort | uniq -c | sort -nr | head -n5 | awk '{print $2 " - " $1 " requests"}'
+
